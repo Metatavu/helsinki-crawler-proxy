b234c734565ff60f8edbc884de5b3b9d9028e803
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-03 11:33:19 +0300

    Added types for Elastic content category

---
+++ src/elastic/types.ts
@@ -0,0 +1,11 @@
+/**
+ * Enum for content category
+ */
+export enum ContentCategory {
+  SERVICE = "service",
+  UNIT = "unit",
+  NEWS = "news",
+  UNCATEGORIZED = "uncategorized",
+  CONTACT = "contact",
+  EXTERNAL = "service_external_link"
+};
