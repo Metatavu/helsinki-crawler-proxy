561a9ef67d3cf05d230507725a3ef59726cf2ce9
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-03 20:47:45 +0300

    SSL support

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import proxyRoutes from "./routes/proxy-routes";
 import https from "node:https";
+import fs from "node:fs";
 import config from "./config";
 
 const app = express();
-const server = https.createServer(config.http.ssl, app);
+const server = https.createServer(
+  {
+    cert: fs.readFileSync(config.http.ssl.cert, "utf8"),
+    key: fs.readFileSync(config.http.ssl.key, "utf8"),
+  },
+  app,
+);
 
 app.use("/", proxyRoutes);
 
