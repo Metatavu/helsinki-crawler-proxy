587c79534ac9db49813c490008682ba8f121838c
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-03 12:59:18 +0300

    Review changes

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,13 @@
-import dotenv from 'dotenv';
-import { cleanEnv, str, num } from 'envalid';
+import dotenv from "dotenv";
+import { cleanEnv, str, num } from "envalid";
 
 dotenv.config();
 
 const env = cleanEnv(process.env, {
-  PROXY_HOST: str({"default": "test-proxy.metatavu.io"}),
-  PROXY_PORT: num({"default": 3128}),
-  PROXY_USERNAME: str({"default": "proxyuser"}),
-  PROXY_PASSWORD: str({"default": "proxypass"})
+  PROXY_HOST: str({ default: "test-proxy.metatavu.io" }),
+  PROXY_PORT: num({ default: 3128 }),
+  PROXY_USERNAME: str({ default: "proxyuser" }),
+  PROXY_PASSWORD: str({ default: "proxypass" }),
 });
 
 export const config = {
@@ -15,6 +15,6 @@ export const config = {
     host: env.PROXY_HOST,
     port: env.PROXY_PORT,
     username: env.PROXY_USERNAME,
-    password: env.PROXY_PASSWORD
+    password: env.PROXY_PASSWORD,
   },
 };
