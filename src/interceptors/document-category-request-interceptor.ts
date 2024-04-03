91329d91c8cc8a1e742e404b5ef98053a4f8c4fa
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-03 11:35:08 +0300

    Added interceptor for adding document categories to www.hel.fi urls

---
+++ src/interceptors/document-category-request-interceptor.ts
@@ -0,0 +1,51 @@
+import * as cheerio from "cheerio";
+import AbstractProxyRequestInterceptor from "./abstract-proxy-request-interceptor";
+import HtmlUtils from "../utils/html-utils";
+import { ContentCategory } from "../elastic/types";
+
+export default class DocumentCategoryRequestInterceptor implements AbstractProxyRequestInterceptor {
+  
+  /**
+   * Document category request interceptor should intercept all www.hel.fi requests but not other domains
+   * 
+   * @param targetUrl target url
+   * @returns whether the interceptor should intercept the request
+   */
+  public shouldIntercept = (targetUrl: string): boolean => {
+    try {
+      const url = new URL(targetUrl); 
+      return url.hostname === "www.hel.fi";
+    } catch (error) {
+      return false;
+    }
+  }
+
+  /**
+   * Intercept and modify the body of the response
+   * 
+   * @param targetUrl target url
+   * @param $ cheerio instance
+   */
+  public intercept = async (targetUrl: string, $: cheerio.CheerioAPI) => {
+    const helfiContentType = HtmlUtils.getMetaTag($, 'helfi_content_type');
+    const contentCategory = this.getContentCategory(helfiContentType);
+
+    HtmlUtils.setMetaTag($, 'elastic:category', contentCategory);
+  }
+
+  /**
+   * Returns content category type from given category string
+   *
+   * @param helfiContentType content type from www.hel.fi -domain meta tag
+   * @returns content category type
+   */
+  private getContentCategory = (helfiContentType: string | null) => {
+    switch (helfiContentType) {
+      case "news_item": return ContentCategory.NEWS;
+      case "tpr_unit": return ContentCategory.UNIT;
+      case "tpr_service": return ContentCategory.SERVICE;
+      default: return ContentCategory.UNCATEGORIZED;
+    }
+  };
+
+};
---
+++ src/interceptors/index.ts
@@ -0,0 +1,5 @@
+import NewsPublishedRequestInterceptor from "./news-published-request-interceptor";
+
+export default [
+  new NewsPublishedRequestInterceptor()
+];
