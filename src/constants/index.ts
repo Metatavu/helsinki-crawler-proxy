f1b9e47a1fc20eb0d0923d0277818f4f5c4cec82
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-08 12:01:01 +0300

    Added list of supported languages

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -9,3 +9,25 @@ export enum ContentCategory {
   CONTACT = "contact",
   EXTERNAL = "service_external_link",
 }
+
+/**
+ * List of supported languages
+ */
+export const SUPPORTED_LANGUAGES = [
+  "de",
+  "en",
+  "es",
+  "et",
+  "fa",
+  "fi",
+  "fr",
+  "it",
+  "la",
+  "lt",
+  "no",
+  "ro",
+  "ru",
+  "sk",
+  "so",
+  "sv",
+];
