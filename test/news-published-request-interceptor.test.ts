5d2a3cdb18786748e4dd412515e1ef4a4ac36309
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-08 12:25:56 +0300

    Tests

--- test/news-published-request-interceptor.test.ts
+++ test/news-published-request-interceptor.test.ts
@@ -1,3 +1,4 @@
+import { DateTime } from "luxon";
 import NewsPublishedRequestInterceptor from "../src/interceptors/news-published-request-interceptor";
 import ResourceUtils from "./utils/resource-utils";
 
@@ -29,7 +30,9 @@ describe("News published request interceptor test suite", () => {
         const { $ } = ResourceUtils.getTestResourceForUrl(url);
         const headers = ResourceUtils.getRequestHeadersForUrl(url);
         await interceptor.intercept(headers, $);
-        expect($('head>meta[name="news_published"]').attr("content")).toBe("2024-03-20T11:45:36.000+02:00");
+        expect($('head>meta[name="news_published"]').attr("content")).toBe(
+          DateTime.fromISO("2024-03-20T11:45:36.000+02:00").toISO(),
+        );
         expect($('head>meta[name="news_published"]').attr("class")).toBe("elastic");
       }),
     );
