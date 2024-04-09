11d4bf858e4a15d4076080c63d8da9d4493a0608
Antti Leppä <antti.leppa@metatavu.fi>
2024-04-09 06:23:51 +0300

    Debug

--- src/interceptors/index.ts
+++ src/interceptors/index.ts
@@ -1,4 +1,11 @@
+import BreadcrumbsRequestInterceptor from "./breadcrumbs-request-interceptor";
 import DocumentCategoryRequestInterceptor from "./document-category-request-interceptor";
+import DocumentLanguageRequestInterceptor from "./document-language-request-interceptor";
 import NewsPublishedRequestInterceptor from "./news-published-request-interceptor";
 
-export default [new DocumentCategoryRequestInterceptor(), new NewsPublishedRequestInterceptor()];
+export default [
+  new DocumentCategoryRequestInterceptor(),
+  new NewsPublishedRequestInterceptor(),
+  new DocumentLanguageRequestInterceptor(),
+  new BreadcrumbsRequestInterceptor(),
+];
